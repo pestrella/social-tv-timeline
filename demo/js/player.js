b0d41c939e0c6959b617f36348e85c7f641565be
Paolo Estrella <pestrella.developer@gmail.com>
2012-01-28 18:52:16 +0000

    scrolling timeline.

--- demo/js/player.js
+++ demo/js/player.js
@@ -6,6 +6,13 @@ $(document).ready(function() {
     }, 5000);
   }
 
+  var timelineScrolling = function(player) {
+    var timeInterval = setInterval(function() {
+      console.log('move it!');
+      $('#lane').animate({"left": "-=20px"}, "slow");
+    }, 5000);
+  }
+
   $('video').mediaelementplayer({
     success: function(player) {
       console.log('playing: ' + player.src);
@@ -13,6 +20,7 @@ $(document).ready(function() {
       /* TODO: requires callback which moves the timeline
        * e.g. timeline.moveTo(player.currentTime);
        */
+      timelineScrolling(player);
     }
   });
 });
