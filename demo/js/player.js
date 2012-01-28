d3676383166de8939429c256571ef4c291079180
Paolo Estrella <pestrella.developer@gmail.com>
2012-01-28 17:43:23 +0000

    log video current time.

--- demo/js/player.js
+++ demo/js/player.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
+  console.log('ready...');
+  var logTime = function(player) {
+    var timeInterval = setInterval(function() {
+      console.log('> ' + player.currentTime);
+    }, 5000);
+  }
 
+  $('video').mediaelementplayer({
+    success: function(player) {
+      console.log('playing: ' + player.src);
+      logTime(player);
+    }
+  });
 });
