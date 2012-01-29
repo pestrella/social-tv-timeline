e4b1433347e5ae0fc2317a8a05f806d3924f696e
Paolo Estrella <pestrella.developer@gmail.com>
2012-01-29 12:27:14 +0000

    bug fixes on scrolling.

--- demo/js/player.js
+++ demo/js/player.js
@@ -16,34 +16,49 @@ $(document).ready(function() {
 
       player.addEventListener('seeked', function(event) {
         //console.log('you seeked!');
-        var videoLength = player.duration;
+        //var videoLength = player.duration;
+        var videoLength = 866; //sec hardcoded lenght! don't change the video :)
         var elapsed = player.currentTime;
         var timelineWidth = $('#lane').width();
         var seekPosition = Math.round((elapsed / videoLength) * timelineWidth);
-        console.log('seek position: ' + seekPosition);
 
         var timelineRemainder = timelineWidth - seekPosition;
         var videoRemainder = Math.round(videoLength - elapsed);
-        console.log('must scroll ' + videoRemainder + 'secs of timeline');
+        console.log('video length: ' + videoLength + 'sec, elapsed time: ' + elapsed + 'sec, timeline should scroll: ' + seekPosition + 'px, remaining timeline: ' + timelineRemainder + 'px, video time remaining: ' + videoRemainder);
 
-        var node = $('#lane');
-        console.log(node);
-        node
+        $('#lane')
           .stop()
           .animate({left: -1 * seekPosition}, {duration: 500})
           .animate({left: -1 * timelineRemainder}, {duration: videoRemainder * 1000});
       });
-    },
-    play: function(player) {
-      console.log('start.');
-      var videoLength = player.duration;
-      var elapsed = player.currentTime;
-      var timelineWidth = $('#lane').width();
 
-      var timelineRemainder = timelineWidth - seekPosition;
-      var videoRemainder = Math.round(videoLength - elapsed);
+      player.addEventListener('play', function(event) {
+        console.log('play >');
+        //var videoLength = player.duration;
+        var videoLength = 866; //sec, hardcoded lenght! don't change the video :)
+        var elapsed = player.currentTime;
+        var timelineWidth = $('#lane').width();
+        var seekPosition = Math.round((elapsed / videoLength) * timelineWidth);
 
-      $('#lane').stop().animate({left: -1 * timelineRemainder}, {duration: videoRemainder * 1000});
+        var timelineRemainder = timelineWidth - seekPosition;
+        var videoRemainder = Math.round(videoLength - elapsed);
+
+        /* massive hack coming up... */
+        var seekPosition = (seekPosition == 0) ? timelineWidth : seekPosition;
+        console.log('video length: ' + videoLength + 'sec, elapsed time: ' + elapsed + 'sec, timeline should scroll: ' + seekPosition + 'px, remaining timeline: ' + timelineRemainder + 'px, video time remaining: ' + videoRemainder);
+
+        $('#lane')
+          .stop()
+          .animate({
+             left: -1 * timelineRemainder
+           }, {
+             duration: videoRemainder * 1000,
+             step: function(now, fx) {
+               //console.log('... we srolling.');
+             }
+           });
+
+      })
     }
   });
 });
